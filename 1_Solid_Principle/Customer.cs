9c4eb0f6b65c559d4f850202b3f5e3062e3db444
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-12-18 11:14:21 +0530

    Solid Principle

--- 1_Solid_Principle/Customer.cs
+++ 1_Solid_Principle/Customer.cs
@@ -6,41 +6,144 @@ using System.Threading.Tasks;
 
 namespace _1_Solid_Principle
 {
-    public class Customer
+    public abstract class Customer
     {
-        public void Insert()
+
+
+        public string customerType { get; set; }
+        /* public void Insert()
+         {
+             try
+             {
+                 int a = 10, b = 0;
+                 Console.WriteLine(a);
+                 int x = a / b;
+
+
+             }
+             catch (Exception ex) {
+
+                 // single responsibility
+                FileLogger fileLogger = new FileLogger();
+                 fileLogger.Log(ex.Message);
+             }
+         }
+
+         public void Update()
+         {
+             try
+             {
+                 int a = 10, b = 0;
+                 Console.WriteLine(a);
+                 int x = a / b;
+
+
+             }
+             catch (Exception ex)
+             {
+
+                 File.AppendAllText(@"logs/Error.txt", $"{ex.Message}\n");
+             }
+         }*/
+
+       // public abstract int GetTicketAmount();
+        public void ShowsTiming()
         {
-            try
-            {
-                int a = 10, b = 0;
-                Console.WriteLine(a);
-                int x = a / b;
-
-           
-            }
-            catch (Exception ex) {
-
-                // single responsibility
-               FileLogger fileLogger = new FileLogger();
-                fileLogger.Log(ex.Message);
-            }
+            Console.WriteLine("*** All Todays Shows ***");
         }
 
-        public void Update()
+      //  public abstract void PrintTicket();
+    }
+
+    //LSP  =>  replace abstract to interface
+    public interface ICustomer    {
+          int GetTicketAmount();
+          void PrintTicket();
+
+        //not add here ISP
+       // void freeFood(); // existing client forced 
+
+    }
+
+    // here i will manage ISP like freefood not force every person
+    public interface Icustomer1 : ICustomer
+    {
+        void freeFood();
+    }
+
+
+    //ocp
+    public class SilverCustomer : Customer, ICustomer
+    {
+        public  int GetTicketAmount()
         {
-            try
-            {
-                int a = 10, b = 0;
-                Console.WriteLine(a);
-                int x = a / b;
+            return 200;
+        }
+
+        public   void PrintTicket()
+        {
+            Console.WriteLine("Silver Ticket Printed");
+        }
+    }
+    public class GoldCustomer : Customer,ICustomer
+    {
+        public  int GetTicketAmount()
+        {
+            return 500;
+        }
+
+        public  void PrintTicket()
+        {
+            Console.WriteLine("Gold Ticket Printed");
+        }
+    }
+
+    public class PlatinumCustomer : Customer,ICustomer
+    {
+        public  int GetTicketAmount()
+        {
+            return 700;
+        }
+
+        public  void PrintTicket()
+        {
+            Console.WriteLine("Platinum Ticket Printed");
+        }
+    }
+
+    public class Enquiry : Customer 
+    {
+       /* public override int GetTicketAmount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PrintTicket()
+        {
+            throw new NotImplementedException();
+        }*/
+    }
 
 
-            }
-            catch (Exception ex)
-            {
+    //ISP  interface most used
 
-                File.AppendAllText(@"logs/Error.txt", $"{ex.Message}\n");
-            }
+    //new client add existing without any forced another customer
+    public class VIPCustomer : Customer, Icustomer1
+    {
+        public int GetTicketAmount()
+        {
+
+            return 1000;
+        }
+
+        public void PrintTicket()
+        {
+            Console.WriteLine("VIP Cutomer Ticket Printed");
+        }
+
+        public void freeFood()
+        {
+            Console.WriteLine("Free Food");
         }
     }
 }
--- 1_Solid_Principle/Program.cs
+++ 1_Solid_Principle/Program.cs
@@ -5,12 +5,55 @@
       public  static void Main(string[] args)
         {
             Console.WriteLine("Program Starts here....");
-            Customer customer = new Customer();
+            // Customer customer = new Customer();
 
-            customer.Insert();
+
+            //Ocp
+            // it is not good way  so use inheritance
+              Console.WriteLine("Enter Customer Type");
+               string customerType = Console.ReadLine().ToUpper();
+            /*
+               if (customerType == "SILVER")
+               {
+                   customer.Insert();
+               }else if (customerType == "Gold")
+               {
+                   customer.Insert();
+               }*/
+            //  customer.Insert();
+
+            ICustomer c=null;
+
+            if (customerType == "SILVER")
+            {
+                c=new SilverCustomer();
+            }
+            else if(customerType =="GOLD")
+            {
+                c=new GoldCustomer();
+            }
+            else if (customerType == "PLATINUM")
+            {
+                c = new GoldCustomer();
+            }
+
+            if (c != null)
+            {
+                c.PrintTicket();
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Customer");
+            }
 
             Console.WriteLine("Program Ends here....");
             Console.ReadLine();
+
+        //    ICustomer obj = new Enquiry();
+
+
+           // why use above because  no change when new customer add we can add separte
         }
     }
 }
